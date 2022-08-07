d1b88aacc557dec6a12d957d6264979dc0cf6897
unknown <liujinshi>
2022-08-07 15:14:42 +0800

    [COMMON]完善大屏

--- src/views/monitorCenter/index.vue
+++ src/views/monitorCenter/index.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2022-07-26 21:40:37
- * @LastEditTime: 2022-08-07 14:09:34
+ * @LastEditTime: 2022-08-07 15:06:03
  * @LastEditors: your name
  * @Description: 
 -->
@@ -20,7 +20,7 @@
              alt="" />
         <div class="join_item_des">
           <div class="join_item_title">单位接入数</div>
-          <div class="join_item_num">169</div>
+          <div class="join_item_num">{{info['unite-count'] || 0}}</div>
         </div>
       </div>
       <div class="screen_join_right screen_join_item">
@@ -28,7 +28,7 @@
              alt="" />
         <div class="join_item_des">
           <div class="join_item_title">本月新增</div>
-          <div class="join_item_num">5</div>
+          <div class="join_item_num">{{info['unite-count-now'] || 0}}</div>
         </div>
       </div>
     </div>
@@ -46,7 +46,7 @@
                 电气火灾监控系统
               </div>
               <div class="wl_module_item_num">
-                1268
+                68
               </div>
             </div>
             <div class="wl_module_item">
@@ -54,7 +54,7 @@
                 独立式烟感系统
               </div>
               <div class="wl_module_item_num">
-                3456
+                345
               </div>
             </div>
             <div class="wl_module_item">
@@ -62,7 +62,7 @@
                 独立式烟感系统
               </div>
               <div class="wl_module_item_num">
-                782
+                78
               </div>
             </div>
             <div class="wl_module_item">
@@ -70,14 +70,14 @@
                 可燃气体系统
               </div>
               <div class="wl_module_item_num">
-                611
+                61
               </div>
             </div>
           </div>
-          <!-- <div style="color:#fff; font-size: 16px">近30天物联接入趋势</div> -->
-          <line-chart class="line_area"
+          <div style="color:#fff; font-size: 16px">近30天物联接入趋势</div>
+          <!-- <line-chart class="line_area"
                       graphNameId="wl"
-                      title="近30天物联接入趋势"></line-chart>
+                      title="近30天物联接入趋势"></line-chart> -->
           <div id="wl_line"></div>
         </div>
         <div class="screen_left_bottom screen_left_item">
@@ -261,9 +261,11 @@
             </div>
           </div>
 
-          <line-chart class="line_area"
+          <!-- <line-chart class="line_area"
                       graphNameId="wl"
-                      title="近30天报警趋势"></line-chart>
+                      title="近30天报警趋势"></line-chart> -->
+          <div style="color:#fff; font-size: 16px;">近30天报警趋势</div>
+          <div id="baojingLine"></div>
         </div>
         <div class="screen_right_bottom screen_right_item">
           <div class="screen_little_title">
@@ -271,14 +273,39 @@
           </div>
           <div class="screen_right_bottom_item">
             <!-- 柱状图 -->
-            <div class="zjt">
+            <div class="zjt"
+                 id="zjt">
 
             </div>
             <div class="tongji">
               <div class="tongji_title">授权使用统计</div>
               <div class="tongji_content">
-                <div class="tongji_content_item item1"></div>
-                <div class="tongji_content_item item2"></div>
+                <div class="tongji_content_item item1">
+                  <div class="content">
+                    <div class="title">语音</div>
+                    <div class="num">
+                      <span class="num1">7/</span>
+                      <span class="num2">5000</span>
+                    </div>
+                    <div class="pre">
+                      <div class="pre_in"
+                           :style="{'width': '10px'}"></div>
+                    </div>
+                  </div>
+                </div>
+                <div class="tongji_content_item item2">
+                  <div class="content">
+                    <div class="title">短信</div>
+                    <div class="num">
+                      <span class="num1">0/</span>
+                      <span class="num2">5000</span>
+                    </div>
+                    <div class="pre">
+                      <div class="pre_in"
+                           :style="{'width': '0px'}"></div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -306,7 +333,10 @@ export default {
       height: document.body.clientHeight / 1080,
       icon: icon,
       show: true,
-      loading: false
+      loading: false,
+      info: {
+
+      }
     }
   },
   methods: {
@@ -437,7 +467,17 @@ export default {
       this.loading = true
       this.$get('/v1/dr/in-big-screen-count').then(res => {
         console.log(res);
+        if (res.code == 200) {
+          for (let key in res.data) {
+            this.$set(this.info, key, res.data[key])
+          }
+        }
+        this.setywPie();
+        this.setwulianLine();
+        this.setbaojingLine();
+        this.setBar()
         this.loading = false
+
       }).catch(err => {
         console.log(err);
         this.$$message.info(err);
@@ -445,8 +485,387 @@ export default {
       })
     },
     setBar () {
+      let barProChart = this.$echarts.init(document.getElementById(`zjt`));
+      let options = {
+        legend: {//图例组件，颜色和名字
+          left: 'center',
+          top: 0,
+          itemGap: 16,
+          itemWidth: 8,
+          itemHeight: 8,
+          data: [{
+            name: '已完成',
+          },
+          {
+            name: '未完成',
+          }],
+          textStyle: {
+            color: '#ffffff',
+            fontStyle: 'normal',
+            fontFamily: 'PingFangSC-Regular',
+            fontSize: 12,
+          }
+        },
+        xAxis: [
+          {
+            type: 'category',
+            boundaryGap: true,//坐标轴两边留白
+            data: ['7/17', '7/22', '7/27', '08/01', '08/06', '08/11', '08/14'],
+            axisLabel: { //坐标轴刻度标签的相关设置。
+              interval: 0,//设置为 1，表示『隔一个标签显示一个标签』
+              margin: 15,
+              textStyle: {
+                color: '#ffffff',
+                fontStyle: 'normal',
+                fontFamily: 'PingFangSC-Regular',
+                fontSize: 12,
+              }
+            },
+            axisTick: {//坐标轴刻度相关设置。
+              show: false,
+            },
+            axisLine: {//坐标轴轴线相关设置
+              lineStyle: {
+                color: '#0A4E85',
+                width: 1
+              }
+            },
+            splitLine: { //坐标轴在 grid 区域中的分隔线。
+              show: false,
+            }
+          },
 
-    } 
+        ],
+        yAxis: [
+          {
+            type: 'value',
+            splitNumber: 4,  // Y轴分割数
+            axisLine: {
+              show: true,
+              lineStyle: {
+                color: '#0A4E85',
+                width: 1
+              }
+            },
+            axisLabel: {
+              color: '#ffffff'
+            },
+            splitLine: {
+              lineStyle: {
+                color: '#0A4E85',
+                opacity: 0.5
+              }
+            }
+          }
+        ],
+        series: [
+          {
+            name: '已完成',
+            type: 'bar',
+            data: [35, 45, 95, 80, 110, 80, 100],
+            barWidth: 7,
+            barGap: 0,//柱间距离
+            itemStyle: {//图形样式
+              normal: {
+                color: new this.$echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                  offset: 0, color: '#00D0C9'
+                },
+                {
+                  offset: 0.7, color: '#094854'
+                }], false),
+              },
+            },
+          },
+          {
+            name: '未完成',
+            type: 'bar',
+            data: [35, 35, 28, 20, 40, 30, 40],
+            barWidth: 7,
+            barGap: 0.5,//柱间距离
+            itemStyle: {//图形样式
+              normal: {
+                color: new this.$echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                  offset: 0.8, color: '#EE6408'
+                },
+                {
+                  offset: 1, color: '#094854'
+                }], false),
+              },
+            },
+          }
+        ]
+      };
+      barProChart.setOption(options);
+      window.addEventListener('resize', function () {
+        //宽度自适应
+        barProChart.resize();
+      });
+    },
+    setwulianLine () {
+      var line_ChartPro = this.$echarts.init(document.getElementById(`wl_line`));
+      let options = {
+        xAxis: {
+          type: 'category',
+          axisTick: {
+            show: false,
+          },
+          axisLine: {
+            lineStyle: {
+              color: '#0A4E85',
+              width: 1
+            }
+          },
+          axisLabel: {
+            color: '#ffffff',
+            fontSize: "12px",
+            fontFamily: 'PingFangSC-Regular'
+          },
+          data: ['7/17', '7/18', '7/19', '7/20', '7/21', '7/22', '7/23', '7/24', '7/25', '7/26', '7/27', '7/28']
+        },
+        yAxis: {
+          type: 'value',
+          axisLine: {
+            show: true,
+            lineStyle: {
+              color: '#0A4E85',
+              width: 1
+            }
+          },
+          axisLabel: {
+            color: '#ffffff'
+          },
+          splitLine: {
+            lineStyle: {
+              color: '#0A4E85',
+              opacity: 0.5
+            }
+          }
+        },
+        series: [
+          {
+            data: [50, 30, 60, 40, 90, 120, 50, 30, 60, 40, 90, 120],
+            type: 'line',
+            smooth: true,
+            symbol: 'none',
+            itemStyle: {
+              color: {
+                type: 'linear',
+                x: 0,
+                y: 0,
+                x2: 1,
+                y2: 0,
+                colorStops: [{
+                  offset: 0, color: '#169DAE ' // 0% 处的颜色
+                }, {
+                  offset: 1, color: '#4CFB9B ' // 100% 处的颜色
+                }],
+                global: false // 缺省为 false
+              }
+            }
+          }
+        ]
+      };
+      line_ChartPro.setOption(options);
+      window.addEventListener('resize', function () {
+        //宽度自适应
+        line_ChartPro.resize();
+      });
+    },
+    setbaojingLine () {
+      let bjline_ChartPro = this.$echarts.init(document.getElementById(`baojingLine`));
+      let options = {
+        xAxis: {
+          type: 'category',
+          axisTick: {
+            show: false,
+          },
+          axisLine: {
+            lineStyle: {
+              color: '#0A4E85',
+              width: 1
+            }
+          },
+          axisLabel: {
+            color: '#ffffff',
+            fontSize: "12px",
+            fontFamily: 'PingFangSC-Regular'
+          },
+          data: ['7/17', '7/18', '7/19', '7/20', '7/21', '7/22', '7/23',]
+        },
+        yAxis: {
+          type: 'value',
+          axisLine: {
+            show: true,
+            lineStyle: {
+              color: '#0A4E85',
+              width: 1
+            }
+          },
+          axisLabel: {
+            color: '#ffffff'
+          },
+          splitLine: {
+            lineStyle: {
+              color: '#0A4E85',
+              opacity: 0.5
+            }
+          }
+        },
+        series: [
+          {
+            data: [50, 30, 60, 40, 90, 120, 50],
+            type: 'line',
+            smooth: true,
+            symbol: 'none',
+            itemStyle: {
+              color: {
+                type: 'linear',
+                x: 0,
+                y: 0,
+                x2: 1,
+                y2: 0,
+                colorStops: [{
+                  offset: 0, color: '#169DAE ' // 0% 处的颜色
+                }, {
+                  offset: 1, color: '#4CFB9B ' // 100% 处的颜色
+                }],
+                global: false // 缺省为 false
+              }
+            }
+          }
+        ]
+      };
+      bjline_ChartPro.setOption(options);
+      window.addEventListener('resize', function () {
+        //宽度自适应
+        bjline_ChartPro.resize();
+      });
+    },
+    lineChart (data, name, unit) {
+      let serve = {
+        name: '',
+        type: 'line',
+        areaStyle: {
+          color: {
+            type: 'linear',
+            x: 0,
+            y: 0,
+            x2: 0,
+            y2: 1,
+            colorStops: [
+              {
+                offset: 0,
+                color: 'red',
+              },
+              {
+                offset: 1,
+                color: 'blue',
+              },
+            ],
+          },
+        },
+        areaStyle: {
+          color: new this.$echarts['graphic'].LinearGradient(
+            0,
+            0,
+            0,
+            1,
+            ecolor[1]
+          ),
+        },
+        itemStyle: {
+          normal: {
+            color: color[0],
+            lineStyle: {
+              color: color[0],
+            },
+          },
+        },
+        data: [],
+        smooth: !0,
+      };
+      let servers = [];
+      data.y.map((it) => {
+        let obj = { ...serve };
+        obj.name = it.name;
+        obj.data = it.data;
+        servers.push(obj);
+      });
+      let option = {
+        tooltip: {
+          trigger: 'axis',
+          backgroundColor: 'rgba(0,0,0,0.7)',
+          borderWidth: '0',
+          axisPointer: {
+            type: 'shadow',
+          },
+          textStyle: {
+            fontSize: 14,
+            color: 'rgba(255,255,255,0.80)',
+          },
+        },
+        legend: {
+          show: false,
+          icon: 'rect',
+          orient: 'horizontal',
+          right: 0,
+          itemWidth: 8,
+          itemHeight: 8,
+          textStyle: {
+            color: 'rgba(255,255,255,0.70)',
+          },
+        },
+        grid: {
+          x: 20,
+          x2: 20,
+          y: 20,
+          y2: 10,
+          containLabel: true,
+        },
+        xAxis: {
+          type: 'category',
+          data: data.x,
+          axisLabel: {
+            show: !0,
+            //interval: 13,
+            fontSize: 12,
+            color: 'rgba(255,255,255,0.40)',
+          },
+        },
+        yAxis: [
+          {
+            type: 'value',
+            minInterval: 1,
+            min: 0,
+            axisTick: {
+              lineStyle: {
+                width: 0,
+              },
+            },
+            axisLine: {
+              lineStyle: {
+                width: 0,
+              },
+            },
+            splitLine: {
+              lineStyle: {
+                width: 0,
+              },
+            },
+            axisLabel: {
+              formatter: '{value}' + unit,
+              color: 'rgba(255,255,255,0.40)',
+              interval: 2,
+            },
+          },
+        ],
+        series: servers,
+        color: color,
+      };
+
+      this.chart = this.$echarts.init(document.getElementById('lineChart'));
+      this.chart.setOption(option, !0);
+    }
   },
   mounted () {
     console.log(icon);
@@ -454,7 +873,7 @@ export default {
     //   this.show = true
     // }, 2000)
     this.getData()
-    this.setywPie()
+
   }
 }
 </script>
