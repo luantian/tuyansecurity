3771d4106bfa9fd74f627e2912a4e24f4e14da21
unknown <liujinshi>
2022-08-07 12:52:50 +0800

    [COMMON]去掉scope

--- src/views/equipment/equipmentManagement/modules/personal.vue
+++ src/views/equipment/equipmentManagement/modules/personal.vue
@@ -672,5 +672,3 @@ export default {
 };
 
 </script>
-<style lang="scss" scoped>
-</style>
--- src/views/equipment/point/point.vue
+++ src/views/equipment/point/point.vue
@@ -190,7 +190,7 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .point-box{
   position: relative;
 }
