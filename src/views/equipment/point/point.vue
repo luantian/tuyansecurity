86bd4fae7f7c215fc4f486a68a16c94049baaf27
unknown <liujinshi>
2022-08-13 00:08:38 +0800

    [COMMON]增加点位设置分页

--- src/views/equipment/point/point.vue
+++ src/views/equipment/point/point.vue
@@ -3,8 +3,7 @@
     :title="`${info.dr_building_floor} 点位图设置`"
     :visible.sync="dialogVisible"
     v-if="dialogVisible"
-    width="70vw"
-    top="50px"
+    :fullscreen="true"
   >
     <div class="cfff flex sp-b">
       <span>选择设备，然后在点位图中标记</span>
@@ -13,8 +12,13 @@
       >
       <span v-show="pointList.length > 0">点击图中标记，可移除设备</span>
     </div>
-    <div class="mt20 point-box">
+    <div class="mt20 point-box flex">
       <div class="dev-list">
+        <div class="search_box">
+          <el-input placeholder="请输入设备名称" size="small" v-model="keyword" clearable @keyup.enter.native="getSearch">
+            <i slot="prefix" class="el-input__icon el-icon-search" @click="getSearch"></i>
+          </el-input>
+        </div>
         <ul>
           <li
             class="align-center"
@@ -27,25 +31,27 @@
           </li>
           <div v-if="!devList.length">暂无可标记设备</div>
         </ul>
+        <el-pagination class="home_pagination" v-show="devList.length > 0" small :pager-count="5" :page-size="20" layout="prev, pager, next" :total="total" @current-change="getDevList"></el-pagination>
       </div>
-      
-      <div class="point-wrap" @click.stop="setPoint($event)" ref="wrap">
-        <img :src="info.dr_building_pic" alt="" style="width: 100%" />
-        <div
-          class="point-marker"
-          @click.stop="del(item, index)"
-          v-for="(item, index) in pointList"
-          :key="index"
-          :style="`left:${item.x};top:${item.y}`"
-        >
-          <el-tooltip
-            class="item"
-            effect="dark"
-            :content="item.dr_device_name"
-            placement="top"
+      <div class="point_wrap_box" @click.stop="setPoint($event)" ref="wrap">
+        <div v-drag class="point-wrap" @click.stop="setPoint($event)" ref="wrap">
+          <img :src="info.dr_building_pic" alt="" style="width:100%" draggable="false" />
+          <div
+            class="point-marker"
+            @click.stop="del(item, index)"
+            v-for="(item, index) in pointList"
+            :key="index"
+            :style="`left:${item.x};top:${item.y}`"
           >
-            <img :src="item.dr_icon" alt="" />
-          </el-tooltip>
+            <el-tooltip
+              class="item"
+              effect="dark"
+              :content="item.dr_device_name"
+              placement="top"
+            >
+              <img :src="item.dr_icon" alt="" />
+            </el-tooltip>
+          </div>
         </div>
       </div>
     </div>
@@ -58,14 +64,12 @@
 </template>
 
 <script>
-
 export default {
-  components:{},
   props: ["build", "all_build", "dr_unit_id"],
   watch: {
     build(val) {
       //当前建筑
-      this.getDevList();
+      this.getDevList(this.page);
       this.getFacilities()
       this.info = { ...val };
       this.pointList = JSON.parse(this.info.dr_building_point || "[]") || [];
@@ -81,6 +85,13 @@ export default {
         }
       });
     },
+    dialogVisible(val){
+      if(val){
+        this.$nextTick(() => {
+          this.$refs['wrap'].addEventListener('mousewheel', this.debounce(this.handleScroll,300), false)||window.addEventListener("DOMMouseScroll",this.debounce(this.handleScroll,300),false)
+        })
+      }
+    }
   },
   data() {
     return {
@@ -90,10 +101,42 @@ export default {
       dev: null,
       pointList: [],
       all_ponit: [],
-      all_dev:[]
+      all_dev:[],
+      scaleNum:1,
+      keyword:'',
+      page:1,
+      total:0
     };
   },
-  created() {},
+  directives: {
+    drag: {
+      // 指令的定义
+      bind: function (el) {
+          let odiv = el;
+          el.onmousedown = (e) => {
+              //算出鼠标相对元素的位置
+              let disX = e.clientX - odiv.offsetLeft+210;
+              let disY = e.clientY - odiv.offsetTop;
+              let left = '';
+              let top = '';
+              odiv.setAttribute('drag-flag',false)
+              document.onmousemove = (e)=>{
+                  //用鼠标的位置减去鼠标相对元素的位置，得到元素的位置
+                  left = e.clientX - disX;
+                  top = e.clientY - disY;
+                  //移动当前元素
+                  odiv.style.left = left + 'px';
+                  odiv.style.top = top + 'px';
+                  odiv.setAttribute('drag-flag',true)
+              };
+              document.onmouseup = (e) => {
+                document.onmousemove = null;
+                document.onmouseup = null;
+              };
+          };
+      }
+    }
+  },
   methods: {
     setPoint(e) {
       // console.log(
@@ -102,13 +145,15 @@ export default {
       //   this.$refs.wrap.offsetWidth,
       //   this.$refs.wrap.offsetHeight
       // );
+      let dragFlag = this.$refs['wrap'].getAttribute('drag-flag');
+      if(dragFlag == 'true'){
+        return;
+      }
       if (!this.dev) {
         return this.$message.info("请先选择设备");
       }
-      this.dev.x =
-        ((e.layerX - 15) / this.$refs.wrap.offsetWidth).toFixed(3) * 100 + "%";
-      this.dev.y =
-        ((e.layerY - 15) / this.$refs.wrap.offsetHeight).toFixed(3) * 100 + "%";
+      this.dev.x = ((e.layerX - 15) / this.$refs.wrap.getBoundingClientRect().width).toFixed(3) * 100 + "%";
+      this.dev.y = ((e.layerY - 15) / this.$refs.wrap.getBoundingClientRect().height).toFixed(3) * 100 + "%";
       this.pointList.push({ ...this.dev });
       this.dev = null;
     },
@@ -123,7 +168,6 @@ export default {
           let ids = this.all_dev.map((it) => {
             return it.dr_device_id;
           });
-          console.log(ids,item.dr_device_id);
 
           if (ids.indexOf(item.dr_device_id) !== -1) {
             this.devList.push(item);
@@ -145,7 +189,13 @@ export default {
     hide() {
       this.dialogVisible = false;
     },
-    getDevList() {
+    //搜索功能
+    getSearch(){
+      this.page = 1;
+      this.getDevList()
+    },
+    getDevList(page) {
+      this.page = page
       this.$post("/v1/dr/device-list-custom", {
         dr_device_name: this.keyword,
         page:this.page
@@ -188,37 +238,71 @@ export default {
         this.$emit("finish");
       });
     },
+    //防抖
+    debounce(func, wait) {
+        let timeout;
+        return function () {
+					let context = this;
+					let args = arguments;
+					if (timeout) clearTimeout(timeout);
+					timeout = setTimeout(() => {
+            func.apply(context, args)
+					}, wait);
+        }
+    },
+    //判断滚动方向
+    handleScroll(e){
+      console.log(11)
+      if(e.deltaY > 0){
+        this.scaleNum -= 0.2;
+      }else{
+        this.scaleNum += 0.2;
+      }
+      this.$refs['wrap'].style.transform = `scale(${this.scaleNum})`
+    }
   },
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+.point_wrap_box{
+  overflow: auto;
+  flex: 1;
+  margin-left: 20px;
+}
 .point-box{
   position: relative;
+  height: calc(100% - 36px);
 }
 .point-wrap {
-  margin-left: 215px;
   position: relative;
   cursor: crosshair;
-  display: flex;
+  transform-origin: 0 0;
+  transition: transform ease-in 0.3s;
   .point-marker {
     position: absolute;
     width: 30px;
     height: 30px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
   }
 }
 .dev-list {
-  padding-left: 15px;
-  width: 200px;
-  position: absolute;
-  max-height: 100%;
-  overflow-y: auto;
-  li {
-    margin-bottom: 10px;
-    cursor: pointer;
+  width: 300px;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  .search_box{
+    padding-bottom: 10px;
+  }
+  ul{
+    flex: 1;
+    overflow-y: auto;
+    li {
+      margin-bottom: 10px;
+      cursor: pointer;
+    }
+  }
+  .home_pagination{
+    text-align: center !important; 
   }
 }
 
